ce67fea4fb6559ccde3f415ae4e18b17017bc979
ioncakephper <shytiger@yahoo.com>
2021-12-20 22:58:51 +0200

    Initial upload

---
+++ tests/sample.test.js
@@ -0,0 +1,70 @@
+describe('UiField', () => {
+    describe('constructor', () => {
+        it('works without parameters', () => {
+            // Your code goes here...
+            expect(true).toBe(true);
+        })
+
+    })
+
+    describe('template method', () => {
+        it('provides empty string when field type unrecognized', () => {
+            // Your code goes here...
+            expect(true).toBe(true);
+        })
+
+        it('provides a template string when field type is specified', () => {
+            // Your code goes here...
+            expect(true).toBe(true);
+        })
+
+        it('provides undefined template filename when type unrecognized', () => {
+            // Your code goes here...
+            expect(true).toBe(true);
+        })
+
+        it('provides template basename when type is specified', () => {
+            // Your code goes here...
+            expect(true).toBe(true);
+        })
+
+    })
+
+})
+
+describe('UiFieldWrapper', () => {
+    describe('render', () => {
+        it('uses attributes from wrapped field', () => {
+            // Your code goes here...
+            expect(true).toBe(true);
+        })
+
+        it('return wrapper code with no content when content is empty', () => {
+            // Your code goes here...
+            expect(true).toBe(true);
+        })
+
+    })
+
+})
+
+describe('TextUiField', () => {
+    describe('render', () => {
+        it('renders value', () => {
+            // Your code goes here...
+            expect(true).toBe(true);
+        })
+
+        it('renders required', () => {
+            // Your code goes here...
+            expect(true).toBe(true);
+        })
+
+        it('renders required and value', () => {
+            // Your code goes here...
+            expect(true).toBe(true);
+        })
+
+    })
+
+})
