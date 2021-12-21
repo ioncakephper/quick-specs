d57c0ff10295bddb70f46912fd5b921b79c9a053
ioncakephper <shytiger@yahoo.com>
2021-12-21 13:36:35 +0200

    Created specifications for quick-specs

---
+++ tests/quick-specs.test.js
@@ -0,0 +1,33 @@
+const qws = require('../index');
+
+describe('quick-specs', () => {
+    describe('load method', () => {})
+
+    describe('buildSpecification method', () => {
+        it('object with suite but no items will render a callback with no content', () => {
+            // Your code goes here...
+            // expect(true).toBe(true);
+          
+            let r = qws.buildSpecification({suite: "", items: []})
+            expect(r).toMatch(/\(\)\s*\=\>\s*\{\s*\}\)/)
+        })
+
+        it('object with suite and items as string will render a callback with it for each item', () => {
+            // Your code goes here...
+            // expect(true).toBe(true);
+
+            let r = qws.buildSpecification({suite: "", items: ["First"]})
+            expect(r).toMatch(/it\(/)
+            
+        })
+
+        it('object with suite and items containing a suite as item will render a callback containing a suite', () => {
+            // Your code goes here...
+            // expect(true).toBe(true);
+            let r = qws.buildSpecification({suite: "", items: [{suite: ""}]})
+            expect(r).toMatch(/\{\n(\s*\n?)*describe/)
+        })
+
+    })
+
+})
